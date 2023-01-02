5a824ca7610581d6bba65c1808799c4becd61aff
ferater <lersmooth@gmail.com>
2023-01-02 16:00:07 +0300

    second project

--- Step Wizard/app.js
+++ Step Wizard/app.js
@@ -1,12 +1,10 @@
 const steps = document.querySelectorAll('.circle');
-// console.log(steps);
+const progress = document.getElementById('progress');
 const prev = document.querySelector('.prev');
-// console.log(prev);
 const next = document.querySelector('.next');
-// console.log(next);
 
 var activeStep = 1;
-
+init();
 
 next.addEventListener('click', () => {
     activeStep++;
@@ -34,11 +32,20 @@ stepUpdate = () => {
         } else {
             step.classList.remove('active');
         }
-        if (steps.length == activeStep) {
-            next.disabled = true;
-            prev.disabled = false;
-        } else if(activeStep <= 1) {
-            next.disabled = false;
-        }
     })
+    init();
+    const actives = document.querySelectorAll('.active');
+    const progresslength = ((actives.length - 1) / (steps.length - 1) * 100 ) + '%';
+    progress.style.width = progresslength;
+}
+
+function init(){
+    if (activeStep == steps.length) {
+        next.disabled = true;
+    } else if(activeStep == 1){
+        prev.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }
